9a1da3479dea2d191c176f8a8096acfbb052372c
Artcs <ju.jeffri.v@gmail.com>
2025-05-11 16:10:26 -0400

    Update run_experiments.sh

--- run_experiments.sh
+++ run_experiments.sh
@@ -72,3 +72,40 @@ python3 finetune.py --model bert-base-multilingual-uncased --context simple --va
 python3 finetune.py --model bert-base-multilingual-uncased --context long --validate
 python3 finetune.py --model bert-base-multilingual-uncased --context short --validate
 python3 finetune.py --model bert-base-multilingual-uncased --context graph --validate
+
+python3 finetune.py --model distilbert-base-multilingual-cased --context None --translate
+python3 finetune.py --model distilbert-base-multilingual-cased --context simple --translate
+python3 finetune.py --model distilbert-base-multilingual-cased --context long --translate
+python3 finetune.py --model distilbert-base-multilingual-cased --context short --translate
+python3 finetune.py --model distilbert-base-multilingual-cased --context graph --translate
+
+python3 finetune.py --model bert-base-multilingual-cased --context None  --translate
+python3 finetune.py --model bert-base-multilingual-cased --context simple --translate  
+python3 finetune.py --model bert-base-multilingual-cased --context long --translate 
+python3 finetune.py --model bert-base-multilingual-cased --context short --translate 
+python3 finetune.py --model bert-base-multilingual-cased --context graph --translate 
+
+python3 finetune.py --model distilbert-base-multilingual-cased --context None --validate --translate 
+python3 finetune.py --model distilbert-base-multilingual-cased --context simple --validate --translate 
+python3 finetune.py --model distilbert-base-multilingual-cased --context long --validate --translate 
+python3 finetune.py --model distilbert-base-multilingual-cased --context short --validate --translate 
+python3 finetune.py --model distilbert-base-multilingual-cased --context graph --validate --translate 
+
+python3 finetune.py --model bert-base-multilingual-cased --context None --validate --translate 
+python3 finetune.py --model bert-base-multilingual-cased --context simple --validate --translate 
+python3 finetune.py --model bert-base-multilingual-cased --context long --validate --translate 
+python3 finetune.py --model bert-base-multilingual-cased --context short --validate --translate 
+python3 finetune.py --model bert-base-multilingual-cased --context graph --validate --translate 
+
+python3 finetune.py --model distilgpt2 --translate --context None --zero_shot
+python3 finetune.py --model distilgpt2 --translate --context simple --zero_shot
+python3 finetune.py --model distilgpt2 --translate --context long --zero_shot
+python3 finetune.py --model distilgpt2 --translate --context short --zero_shot
+python3 finetune.py --model distilgpt2 --translate --context graph --zero_shot
+
+python3 finetune.py --model gpt2 --translate --context None --zero_shot
+python3 finetune.py --model gpt2 --translate --context simple --zero_shot
+python3 finetune.py --model gpt2 --translate --context long --zero_shot
+python3 finetune.py --model gpt2 --translate --context short --zero_shot
+python3 finetune.py --model gpt2 --translate --context graph --zero_shot
+
